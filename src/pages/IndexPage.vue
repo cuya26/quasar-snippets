715ea135c69a748b36336d2b1a38c077e8b5026a
giuseppe serna <giuseppe.serna@polimi.it>
2023-05-30 16:55:41 +0200

    add chatbot interface

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page class="flex flex-center">
+    <div
+      @drop.prevent="this.dropFunction"
+      @dragover.prevent
+      @dragenter.prevent
+      class="bg-primary row items-center justify-center text-white"
+      style="width: 300px; height: 300px"
+    >
+      Drop Zone
+    </div>
+    <div style="width: 50px"></div>
+    <embed
+      :src="dropzoneFile"
+      style="width: 400px; height: 650px"
+      class="bg-teal"
+      type="application/pdf"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  setup() {
+    dropzoneFile: ref("");
+  },
+  methods: {
+    dropFunction(dragEvent) {
+      this.dropzoneFile = URL.createObjectURL(dragEvent.dataTransfer.files[0]);
+      console.log(dragEvent.dataTransfer);
+      console.log(this.dropzoneFile);
+    },
+  },
+});
+</script>
